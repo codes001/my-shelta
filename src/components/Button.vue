511ea1dc24b97e13592e26e8138449e9b3004bc0
charleskaiglo <charlesamos003@gmail.com>
2024-07-21 22:45:02 +0100

    Nav ready

---
+++ src/components/Button.vue
@@ -0,0 +1,50 @@
+<template>
+  <button
+    :class="type"
+    :disabled="disabled"
+    
+  >
+    {{ label }}
+  
+  </button>
+</template>
+
+<style scoped>
+button {
+  padding: 10px 12px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  font-weight: 600;
+  cursor: pointer;
+  background: var(--primary);
+  /* font-size: 16px; */
+}
+
+button[disabled] {
+  background-color: lightgrey;
+  color: darkgrey;
+  cursor: not-allowed;
+}
+</style>
+
+<script setup>
+import { defineProps} from 'vue';
+
+// Define props
+const props = defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    default: 'button' // button, submit, reset
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  }
+});
+</script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,64 @@
-<style scoped></style>
+
+<style scoped>
+
+nav{
+  box-shadow: 1px 3px 10px rgba(239, 231, 224, 0.7);
+  padding: .5rem 0;
+  height: 70px;
+}
+nav .container{
+  justify-content: space-between;
+  align-items: center;
+  
+}
+
+.navbar-ul{
+  gap: 1rem;
+  align-items: center;
+}
+
+@media(max-width:768px){
+  .nav .navbar-ul{
+    display: none;
+  }
+}
+</style>
 
 <template>
-  <nav class="nav">
-    <div class="container">
+  <nav class="nav ">
+    <div class="container flex">
       <RouterLink to="/">
         <img src="/myshelta.jpeg" class="logo" alt="myShelta" />
       </RouterLink>
     
 
-      <ul>
-        <li class="flex" v-for="link in navbarLinks" :key="link.text">
+      <ul class="flex navbar-ul">
+        <li v-for="link in navbarLinks" :key="link.text">
           <RouterLink :to="link.url">{{ link.text }}</RouterLink>
         </li>
+      
+        <WhiteButton
+        label="Sign Up"
+        type="button"
+      />  
+
+      <Button
+        label="Sign Up"
+        type="button"
+      />    
       </ul>
+
+      <!-- <div class="menu">
+        <img src="src/assets/hamburger.svg" alt="">
+      </div> -->
     </div>
   </nav>
 </template>
 
 <script setup>
 import { navbarLinks } from "@/utils";
+import Button from '@/components/Button.vue'
+import WhiteButton from '@/components/WhiteButton.vue'
+// import hamburger from '@/assets/hamburger.svg'
 
 </script>
