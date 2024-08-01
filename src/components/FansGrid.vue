c62ce9c97cb1ba66baba096f9f0255cfa0ada59c
codes001 <prynzwill147@gmail.com>
2024-08-01 17:12:13 +0100

    Commit

--- src/components/FansGrid.vue
+++ src/components/FansGrid.vue
@@ -1,32 +1,57 @@
 <template>
   <section class="w-full">
-    <div class="fans-grid">
-      <h2>Loved by 10,000+ Africans</h2>
+    <div class="container fans-grid">
+      <div class="grid-item grid-item-1">
+        <h2>Loved by 10,000+ Africans</h2>
 
-      <hr style="width: 100px; text-align: center" />
+        <hr style="width: 100px; text-align: center" />
 
-      <Button label="Create a free account" type="button" />
-    </div>
+        <Button label="Create a free account" type="button" />
+      </div>
 
-    <div class="fans-grid">
-      <img :src="a" alt="a" />
-      <img :src="b" alt="b" />
-      <img :src="c" alt="c" />
-      <img :src="d" alt="d" />
-      <img :src="e" alt="e" />
-      <img :src="f" alt="f" />
-      <img :src="g" alt="g" />
-      <img :src="h" alt="h" />
-      <img :src="i" alt="i" />
-      <img :src="j" alt="j" />
-      <img :src="k" alt="k" />
-      <img :src="l" alt="l" />
-      <img :src="m" alt="m" />
+      <div class="fans-grid-main">
+        <img class="grid-item" :src="a" alt="a" />
+        <img :src="b" alt="b" />
+        <img :src="c" alt="c" />
+        <img :src="d" alt="d" />
+        <img :src="e" alt="e" />
+        <img :src="f" alt="f" />
+        <img :src="g" alt="g" />
+        <img :src="h" alt="h" />
+        <img :src="i" alt="i" />
+        <img :src="j" alt="j" />
+        <img :src="k" alt="k" />
+        <img :src="l" alt="l" />
+        <img :src="m" alt="m" />
+      </div>
     </div>
   </section>
 </template>
 <style scoped>
 .fans-grid {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr); /* 6 columns */
+  grid-template-rows: repeat(4, 1fr); /* 4 rows */
+  gap: 10px; /* Add some space between grid items */
+  width: 100%;
+  height: 100vh; /* Full height for demonstration */
+}
+/* 
+.grid-item {
+  background-color: lightblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #ccc;
+  font-size: 20px;
+} */
+
+.grid-item-1 {
+  background: #000a2f;
+  /* width: 100%;
+  height: 100%; */
+  grid-column: 1 / span 3; /* Start at column 1 and span 3 columns */
+  grid-row: 1 / span 3;
 }
 </style>
 
