fc1ad3751272bc1582e2e58ff6396759fd85c068
codes001 <prynzwill147@gmail.com>
2024-08-08 08:45:44 +0100

    Linked Navbar Whatsapp and mail

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,19 +9,18 @@
     <div class="container flex-between">
       <RouterLink class="flex-between header-mail" to="/">
         <img :src="mail" alt="" />
-        <p>hello@myshelta.com</p>
+        <a href="mailto:hello@myshelta.com">hello@myshelta.com</a>
       </RouterLink>
 
       <div class="flex header-mail">
         <img :src="whatsapp" alt="" />
-        <a href="#"> +(234) 8061796909 </a>
-        
+        <a href="https://wa.me/2348061796909?text="> +(234) 8061796909 </a>
       </div>
     </div>
   </header>
 </template>
 
 <script setup>
-import mail from '@/assets/mail.svg';
-import whatsapp from '@/assets/whatsapp.svg';
+import mail from "@/assets/mail.svg";
+import whatsapp from "@/assets/whatsapp.svg";
 </script>
